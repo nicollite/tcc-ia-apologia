f40ed4415558f1af0bab33779194a3e670431ae4
Nicolas Conte <nicolas.conte@alest.com.br>
2020-11-14 00:02:37 -0300

    Change app css and logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,49 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import { api } from "./services/api";
 
 function App() {
-  const [text, setText] = useState("")
-  const [payload, setPayload] = useState([])
+  const [text, setText] = useState("");
+  const [spinner, setSpinner] = useState(false);
+  const [payload, setPayload] = useState([]);
 
   const requestApi = async () => {
-    console.log(text)
-    const res = await api.post("", { text })
-    setPayload(res.data.payload)
-    console.log(res)
-  }
+    if (text.length === 0) return;
+    setSpinner(true);
+    const res = await api.post("", { text });
+    setSpinner(false);
+    setPayload(res.data.payload);
+  };
 
-  const result = payload.map((data, index) => (
-    <span>{data.displayName}: {(parseFloat(data.classification.score) * 100).toFixed(2)} %</span>
-  ))
+  const result = payload.map(data => {
+    const result = `${(parseFloat(data.classification.score) * 100).toFixed(2)}%`;
+    console.log(result);
+
+    return (
+      <div>
+        <span>
+          {data.displayName}: {result}
+        </span>
+        <div className="result-bars bg-gray-div">
+          <div className="result-bars classification-result-div" style={{ width: result }}></div>
+        </div>
+      </div>
+    );
+  });
+
+  const spinnedElement = <div className="loader">Consultando...</div>;
 
   return (
     <div className="bg">
       <div className="main">
         <label htmlFor="text">Insira o texto para ser analisado abaixo:</label>
-        <textarea id="text" onChange={(e) => setText(e.target.value)} />
-        <button className="btn" onClick={requestApi}>{"Enviar >"}</button>
+        <textarea id="text" onChange={e => setText(e.target.value)} />
+        <button className="btn" onClick={requestApi}>
+          {"Enviar >"}
+        </button>
         <div className="result">
           <span>Resultados:</span>
-          {result}
+          {spinner ? spinnedElement : result}
         </div>
       </div>
     </div>
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,3 @@
 import axios from "axios";
 
-console.log({backendUrl:process.env.REACT_APP_BACKEND_URL })
 export const api = axios.create({ baseURL: process.env.REACT_APP_BACKEND_URL });
